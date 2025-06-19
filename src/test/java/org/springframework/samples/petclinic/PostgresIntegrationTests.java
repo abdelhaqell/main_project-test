21bf45d6f48026a4b99e093ff1eb76f59d0b2715
root <root@RAB4D7HK13>
2025-06-20 00:47:09 +0100

    Fix 1 issues in src/test/java/org/springframework/samples/petclinic/PostgresIntegrationTests.java

--- src/test/java/org/springframework/samples/petclinic/PostgresIntegrationTests.java
+++ src/test/java/org/springframework/samples/petclinic/PostgresIntegrationTests.java
@@ -79,7 +79,7 @@ public class PostgresIntegrationTests {
 	}
 
 	@Test
-	void testFindAll() throws Exception {
+	void testFindAll() throws IllegalArgumentException {
 		vets.findAll();
 		vets.findAll(); // served from cache
 	}
@@ -109,42 +109,4 @@ public class PostgresIntegrationTests {
 		}
 
 		public void printProperties() {
-			for (EnumerablePropertySource<?> source : findPropertiesPropertySources()) {
-				log.info("PropertySource: " + source.getName());
-				String[] names = source.getPropertyNames();
-				Arrays.sort(names);
-				for (String name : names) {
-					String resolved = environment.getProperty(name);
-
-					assertNotNull(resolved, "resolved environment property: " + name + " is null.");
-
-					Object sourceProperty = source.getProperty(name);
-
-					assertNotNull(sourceProperty, "source property was expecting an object but is null.");
-
-					assertNotNull(sourceProperty.toString(), "source property toString() returned null.");
-
-					String value = sourceProperty.toString();
-					if (resolved.equals(value)) {
-						log.info(name + "=" + resolved);
-					}
-					else {
-						log.info(name + "=" + value + " OVERRIDDEN to " + resolved);
-					}
-				}
-			}
-		}
-
-		private List<EnumerablePropertySource<?>> findPropertiesPropertySources() {
-			List<EnumerablePropertySource<?>> sources = new LinkedList<>();
-			for (PropertySource<?> source : environment.getPropertySources()) {
-				if (source instanceof EnumerablePropertySource enumerable) {
-					sources.add(enumerable);
-				}
-			}
-			return sources;
-		}
-
-	}
-
-}
+			for (
