ff3be54484b17d55a2df883bbc629546db05d7c2
abdelhaqell <aelattar@sqli.com>
2025-07-21 16:25:41 +0100

    Fix 1 issues in src/main/java/org/springframework/samples/petclinic/owner/PetController.java

--- src/main/java/org/springframework/samples/petclinic/owner/PetController.java
+++ src/main/java/org/springframework/samples/petclinic/owner/PetController.java
@@ -72,9 +72,8 @@ class PetController {
 		}
 
 		Optional<Owner> optionalOwner = this.owners.findById(ownerId);
-		Owner owner = optionalOwner.orElseThrow(() -> new IllegalArgumentException(
-				"Owner not found with id: " + ownerId + ". Please ensure the ID is correct "));
-		return owner.getPet(petId);
+		return optionalOwner.orElseThrow(() -> new IllegalArgumentException(
+				"Owner not found with id: " + ownerId + ". Please ensure the ID is correct ")).getPet(petId);
 	}
 
 	@InitBinder("owner")
